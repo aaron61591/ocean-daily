61e9dc4386a4d9c54bd972475f5de76dbb5c8cd9
aaron <aaron@UCdeMacBook-Pro.local>
2018-01-25 09:58:03 +0800

    按环境区分数据库配置

--- config/config.default.js
+++ config/config.default.js
@@ -1,33 +1,5 @@
 'use strict';
 
-// module.exports = appInfo => {
-//   const config = {};
-//   config.
-//   return config;
-// };
-
-exports.keys = '写一个随机数，不要告诉别人哈';
-
-exports.mysql = {
-  // 单数据库信息配置
-  client: {
-    // host
-    host: 'localhost',
-    // 端口号
-    port: '3306',
-    // 用户名
-    user: 'root',
-    // 密码
-    password: '',
-    // 数据库名
-    database: 'ocean_daily',
-  },
-  // 是否加载到 app 上，默认开启
-  app: true,
-  // 是否加载到 agent 上，默认关闭
-  agent: false,
-};
-
 exports.security = {
   csrf: {
     ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
